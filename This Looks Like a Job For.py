850d6167425e107ee0e43da7bccff791ddebe397
Serendipity31 <Serendipity31@users.noreply.github.com>
2016-02-28 04:08:04 +0000

    CodeAcademy: Uses super to retrieve previous method that had been overridden

---
+++ This Looks Like a Job For.py
@@ -0,0 +1,24 @@
+#This code demonstrates how to use a super function to return a function associated with 
+#the parent class. 
+#Exercise is Introduction to Classes, lesson 14: "This Looks Like a Job For..."
+
+class Employee(object):
+    """Models real-life employees!"""
+    def __init__(self, employee_name):
+        self.employee_name = employee_name
+
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 20.00
+
+# Add your code below!
+class PartTimeEmployee(Employee):
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 12.00
+           
+    def full_time_wage(self, hours):
+       return super(PartTimeEmployee, self).calculate_wage(hours)
+        
+milton = PartTimeEmployee("Milton")
+print milton.full_time_wage(10)
