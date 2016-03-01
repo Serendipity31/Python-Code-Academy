fcac899350f69e2388acc0d94ca9070ecc061c8e
Serendipity31 <Serendipity31@users.noreply.github.com>
2016-03-01 02:58:07 +0000

    CodeAcademy: Adds electric car class to code and an instance of an electric car

--- Car Class and Object.py
+++ Car Class and Object.py
@@ -24,3 +24,12 @@ my_car = Car("DeLorean", "silver", 88)
 print my_car.condition
 my_car.drive_car()
 print my_car.condition
+
+class ElectricCar(Car):
+    def __init__(self, model, color, mpg, battery_type):
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+        self.battery_type = battery_type
+
+my_car2 = ElectricCar("DeLorean", "silver", 88, "molten salt")
