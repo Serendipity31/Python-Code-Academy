1e4c1e63afd91d488a07f22dc95ac669aa932872
Serendipity31 <Serendipity31@users.noreply.github.com>
2016-03-01 02:11:28 +0000

    CodeAcademy: Adds a display_car method to describe the car

--- Car Class and Object.py
+++ Car Class and Object.py
@@ -6,9 +6,14 @@ class Car(object):
         self.model = model
         self.color = color
         self.mpg = mpg
-
+	def display_car(self):
+		#Note that have to use self within the method display_car so that it knows to what the method refers
+		return "This is a %s %s with %s MPG." % (self.color, self.model, str(self.mpg))
+		#Note that here have to use self. to call the member variables, even within the class
+		
 my_car = Car("DeLorean", "silver", 88)
-print my_car.condition
-print my_car.model
-print my_car.color
-print my_car.mpg
+#print my_car.condition
+#print my_car.model
+#print my_car.color
+#print my_car.mpg
+print my_car.display_car()
